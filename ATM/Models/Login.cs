c087d419e13a2bcd5c6b620993802d93ffe98c7f
Pc One Tijuana <Pc One Tijuana@DESKTOP-E4MRRJP>
2021-11-21 14:46:39 -0600

    Agregar archivos de proyecto.

---
+++ ATM/Models/Login.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ATM.Models
+{
+    public class Login
+    {
+        [Required(ErrorMessage ="Favor de agregar un usuario")]
+        public string usuario { get; set; }
+
+        [Required(ErrorMessage ="Favor de agregar una contraseña")]
+        [DataType(DataType.Password)]
+        [StringLength(8,ErrorMessage ="La contraseña debe de tener 8 caracteres ",MinimumLength =8)]
+        public string password { get; set; }
+
+
+    }
+}
